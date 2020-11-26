f13b7a925d6d56acae28aa8c713743e78d9de2c3
satyadudi <66017901+satyadudi@users.noreply.github.com>
2020-11-26 18:14:15 +0530

    17BCE0071

---
+++ BUTTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BUTTO : MonoBehaviour
+{
+    public void Button(string newBP){
+        SceneManagement.LoadScene(newBP);
+    }
+}
